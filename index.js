e12744bf6494a567fce97bd988007e939deecf12
Savannah Feder <sfeder0@gmail.com>
2022-04-28 19:13:46 -0700

    moved around elements

--- index.js
+++ index.js
@@ -4,13 +4,15 @@ fetch(
   .then((res) => res.json())
   .then((data) => {
     document.body.style.backgroundImage = `url(${data.urls.regular})`;
-    document.getElementById("author").textContent = `By: ${data.user.name}`;
+    document.getElementById(
+      "photographer"
+    ).textContent = `Photo by ${data.user.name}`;
   })
   .catch((err) => {
     // Use a default background image/author
     document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1560008511-11c63416e52d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDIxMTc&ixlib=rb-1.2.1&q=80&w=1080
 )`;
-    document.getElementById("author").textContent = `By: Dodi Achmad`;
+    document.getElementById("photographer").textContent = `By: Dodi Achmad`;
   });
 
 const getCurrentTime = () => {
@@ -50,9 +52,8 @@ fetch("https://stoic-server.herokuapp.com/search/marcus")
     console.log(data.length);
     const index = getRandomNumber();
     const currentQuote = data[index];
-    console.log(currentQuote.body);
-    console.log(currentQuote.author);
     document.getElementById("quote").textContent = `"${currentQuote.body}"`;
+    document.getElementById("author").textContent = currentQuote.author;
   });
 
 // returns number between 0 and 515 (516 total MA quotes)
