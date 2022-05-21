878a45b556b9ba176008db8f36870e9cecc7f12f
Savannah Feder <sfeder0@gmail.com>
2022-05-20 20:46:58 -0700

    styling changes + reworking quote retreival

--- index.js
+++ index.js
@@ -1,19 +1,33 @@
 // NEXT STEPS
-// - refactor from fetch to use async await
-// - make quotes render once every day
-// - error catching for quote API
-// - make fonts more readable
+// - cache quotes and images to reduce load time (see how other apps do it)
 // - publish to chrome web store!
 // --------
+// - refactor from fetch to use async await
 // - button to generate new quote and image (small 🔁 button in corner?)
 // - cache quotes or have a json file with all quotes downloaded
 // - ability to choose which stoic author
 //    -> saves the author in local storage
 
+// const quotesLeftInStorage = JSON.parse(localStorage.getItem('quotes'));
+
+// <==================== TIME =======================>
+
 const date = new Date();
 document.getElementById('time').textContent = date.toLocaleTimeString('en-us', {
   timeStyle: 'short',
 });
+
+const getCurrentTime = () => {
+  const todaysDate = new Date();
+  document.getElementById('time').textContent = todaysDate.toLocaleTimeString(
+    'en-us',
+    { timeStyle: 'short' }
+  );
+};
+
+setInterval(getCurrentTime, 1000);
+
+// <=============== BACKGROUND PHOTO =================>
 
 fetch(
   'https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature'
@@ -33,18 +47,26 @@ fetch(
     document.getElementById('photographer').textContent = 'By: Dodi Achmad';
   });
 
-const getCurrentTime = () => {
-  const todaysDate = new Date();
-  document.getElementById('time').textContent = todaysDate.toLocaleTimeString(
-    'en-us',
-    { timeStyle: 'short' }
-  );
+// <================== QUOTES ====================>
+
+const backupQuotes = [
+  {
+    body: `The happiness of your life depends upon the quality of your thoughts: therefore, guard accordingly, and take care that you entertain no notions unsuitable to virtue and reasonable nature.`,
+    author: `Marcus Aurelius`,
+  },
+  {
+    body: `The best revenge is to be unlike him who performed the injury.`,
+    author: `Marcus Aurelius`,
+  },
+  {
+    body: `Our life is what our thoughts make it.`,
+    author: `Marcus Aurelius`,
+  },
+];
+
+const getRandomIndex = (arrayLength) => {
+  return Math.round(Math.random() * (arrayLength - 1));
 };
-
-setInterval(getCurrentTime, 1000);
-
-// returns number between 0 and 515 (516 total MA quotes)
-const getRandomIndex = () => Math.round(Math.random() * 515);
 
 const checkIfQuoteTooLong = (quote) => {
   const maxQuoteChars = 155;
@@ -52,13 +74,51 @@ const checkIfQuoteTooLong = (quote) => {
   return quoteLength > maxQuoteChars;
 };
 
-fetch('https://stoic-server.herokuapp.com/search/marcus')
-  .then((res) => res.json())
-  .then((data) => {
-    let currentQuoteData = data[getRandomIndex()];
-    while (checkIfQuoteTooLong(currentQuoteData)) {
-      currentQuoteData = data[getRandomIndex()];
-    }
-    document.getElementById('quote').textContent = `"${currentQuoteData.body}"`;
-    document.getElementById('author').textContent = currentQuoteData.author;
-  });
+const fetchQuotes = async () => {
+  try {
+    let response = await fetch(
+      'https://stoic-server.herokuapp.com/search/marcus'
+    );
+    return await response.json();
+  } catch (err) {
+    console.log(err);
+    return backupQuotes;
+  }
+};
+
+const displayQuote = async () => {
+  const data = await fetchQuotes();
+  console.log(data);
+  const numQuotes = data.length;
+  let currentQuoteData = data[getRandomIndex(numQuotes)];
+  while (checkIfQuoteTooLong(currentQuoteData)) {
+    currentQuoteData = data[getRandomIndex(numQuotes)];
+  }
+  document.getElementById('quote').textContent = `"${currentQuoteData.body}"`;
+  document.getElementById('author').textContent = currentQuoteData.author;
+};
+
+displayQuote();
+
+const saveNQuotesToLocalStorage = async (n) => {
+  // TODO: fetch quotes and save them below
+  const quotessss = await fetchQuotes();
+  const quotes = ['quote', 'quote2', 'quote3'];
+  const quotesToStore = [];
+  const numQuotes = quotes.length;
+  for (let i = 0; i < n; i++) {
+    const randomIndex = getRandomIndex(numQuotes);
+    const randomQuote = quotes[randomIndex];
+    quotes.push(randomQuote);
+  }
+  localStorage.setItem('savedQuotes', JSON.stringify(quotesToStore));
+};
+
+const retreiveQuoteFromLocalStorage = () => {
+  // replace array in local storage with one w/o just-retreived quote
+  // update quotesLeftInStorage length
+};
+
+// if (quotesLeftInStorage.length <= 1) {
+//   saveNQuotesToLocalStorage(10);
+// }
